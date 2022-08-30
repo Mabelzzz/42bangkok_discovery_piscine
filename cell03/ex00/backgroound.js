051a5f2b793d162bd1149a5f31be3321cee2ab18
Phatcharamon Namwayko <pnamwayk@z3t10c1.42bangkok.com>
2022-08-31 02:12:28 +0700

    finish

--- cell03/ex00/backgroound.js
+++ cell03/ex00/backgroound.js
@@ -1,7 +1,5 @@
 function bgColors()
 {
-    // const randomColors = Math.floor(Math.random()*255);
-    // document.body.style.backgroundColor = colors[randomColors];
     document.body.style.backgroundColor = 
     'rgb(' + Math.floor(Math.random() * 255) + ','
     + Math.floor(Math.random() * 255) + ','
